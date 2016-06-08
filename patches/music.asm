b2f1c0903a0eb6d6dfbdbc683715df2ad7e5f0e6
Wodka <domi.erdnuss2@gmx.de>
2016-06-08 23:19:04 +0200

    Music structure added entirely

--- patches/music.asm
+++ patches/music.asm
@@ -1,9 +1,162 @@
 music_table equ 0x9194500
 
+
+.org music_table + 8*265
+    .word mus_rin_battle
+    .word 0
+
+.org music_table + 8*274
+    .word mus_encounter_violet
+    .word 0
+
+.org music_table + 8*276
+    .word mus_deoxys_battle
+    .word 0
+
+.org music_table + 8*278
+    .word mus_titlescreen
+    .word 0
+
+.org music_table + 8*280
+    .word mus_graveyard
+    .word 0
+
+.org music_table + 8*283
+    .word mus_encounter_beethoven
+    .word 0
+
+.org music_table + 8*284
+    .word mus_encounter_staccato
+    .word 0
+
+.org music_table + 8*285
+    .word mus_encounter_cello
+    .word 0
+
+.org music_table + 8*287
+    .word mus_silvania_forest
+    .word 0
+
+.org music_table +8*291
+    .word mus_new_day
+    .word 0
+
+.org music_table + 8*292
+    .word mus_melancholy
+    .word 0
+
+.org music_table + 8*296
+    .word mus_gym_leader
+    .word 0
+
+.org music_table + 8*297
+    .word mus_trainer_battle
+    .word 0
+
+.org music_table + 8*298
+    .word mus_wild_battle
+    .word 0
+
+.org music_table + 8*300
+    .word mus_amonia
+    .word 0
+
+.org music_table + 8*301
+    .word mus_jorney
+    .word 0
+
+.org music_table + 8*303
+    .word mus_pokecenter
+    .word 0
+
+.org music_table + 8*307
+    .word mus_violet_headquarter
+    .word 0
+
+.org music_table + 8*313
+    .word mus_kaskada_unused
+    .word 0
+
+.org music_table + 8*314
+    .word mus_meriana_city
+    .word 0
+
+.org music_table + 8*315
+    .word mus_encounter_rival
+    .word 0
+
+.org music_table + 8*327
+    .word mus_volcano
+    .word 0
+
+.org music_table + 8*328
+    .word mus_darkness
+    .word 0
+
+.org music_table + 8*330
+    .word mus_summit
+    .word 0
+
+.org music_table + 8*331
+    .word mus_cave
+    .word 0
+
 .org music_table + 8*334
-	.word mus_asterisk_castle
-	.byte 0
-	.byte 0
-	.byte 0
-	.byte 0
-	.pool
+    .word mus_asterisk_castle
+    .word 0
+
+.org music_table + 8*335
+    .word mus_silvania
+    .word 0
+
+.org music_table + 8*336
+    .word mus_aktania
+    .word 0
+
+.org music_table + 8*347
+    .word mus_battle_violet
+    .word 0
+
+.org music_table + 8*348
+    .word sound_step
+    .word 0x10001
+
+.org music_table + 8*349
+    .word mus_jeropardy
+    .word 0
+
+.org music_table + 8*350
+    .word mus_trainerschool
+    .word 0
+
+.org music_table + 8*351
+    .word mus_rival_battle
+    .word 0
+
+.org music_table + 8*352
+    .word mus_desert
+    .word 0
+
+.org music_table + 8*353
+    .word mus_revolution_battle
+    .word 0
+
+.org music_table + 8*354
+    .word mus_kaskada
+    .word 0
+
+.org music_table + 8*355
+    .word mus_revolution
+    .word 0
+
+.org music_table + 8*356
+    .word mus_groudon_battle
+    .word 0
+
+.org music_table + 8*357
+    .word mus_fur_elise
+    .word 0
+
+.org music_table + 8*358
+    .word mus_dimensional_traveler
+    .word 0
