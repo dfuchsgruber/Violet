6da6263afa4043337d4dda94ef5139f97c72d488
WodkaRHR <domi.erdnuss2@gmx.de>
2018-06-13 13:24:53 +0200

    Removed strings

--- string/selection.asm
+++
@@ -1,9 +0,0 @@
-.ifdef LANG_GER
-.global str_trainerschool_selection_text
-
-str_trainerschool_selection_text:
-	.string "  KEY_A  Bestätigen              KEY_B  Abbruch"
-
-
-.elseif LANG_EN
-.endif
