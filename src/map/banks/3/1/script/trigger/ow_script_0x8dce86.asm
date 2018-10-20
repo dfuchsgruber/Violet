cf280a9eaf2765cf56ee89f56cda94cb31bdc66f
Manuel Thalmann <m@nuth.ch>
2018-10-20 15:49:46 +0200

    Rework an optional fix

--- src/map/banks/3/1/script/trigger/ow_script_0x8dce86.asm
+++ src/map/banks/3/1/script/trigger/ow_script_0x8dce86.asm
@@ -84,7 +84,7 @@ str_polizist:
 .global str_0x8dcf8d
 
 str_0x8dcf8d:
-	.autostring 36 2 "Denken Sie, dass sie damit durchkommen?\pWenn die Top Vier von Ihren schmutzigen Geschäften erfährtDOTS"
+	.autostring 36 2 "Denken Sie, dass sie damit durchkommen?\pWenn die Top Vier von Ihren schmutzigen Geschäften erfahrenDOTS"
         
         
 .global str_0x8de230
