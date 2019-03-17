34b651c53a4561cba622f443fa470cb9afe4c378
Manuel Thalmann <m@nuth.ch>
2019-03-17 21:03:52 +0100

    Move mugshot-macros to a separate file

--- Violet/include/as/overworld_script.s
+++ Violet/include/as/overworld_script.s
@@ -1,8 +1,3 @@
-.include "callstds.s"
-.include "mugshot_character.s"
-.include "mugshot_alignment.s"
-.include "vars.s"
-
 .macro nop 
 .byte 0x0
 .endm
@@ -1260,45 +1255,4 @@
 .byte 0xD5
 .hword \persid
 .hword \movement
-.endm
-
-# Custom Macros
-.macro load_mugshot person alignment=MUGSHOT_LEFT mask_name=0
-    setvar 0x8000 \alignment
-    setvar 0x8001 \person
-
-    .if \mask_name == 0x1
-        setvar 0x8002 0x0
-    .else
-        setvar 0x8002 0x1
-    .endif
-.endm
-
-.macro call_draw_mugshot
-    special 0x6
-.endm
-
-.macro call_hide_mugshot
-    special 0x7
-.endm
-
-.macro show_mugshot_message message_type=MSG
-    call_draw_mugshot
-    callstd \message_type
-    call_hide_mugshot
-.endm
-
-.macro draw_mugshot person alignment=MUGSHOT_LEFT mask_name=0
-    load_mugshot \person \alignment \mask_name
-    call_draw_mugshot
-.endm
-
-
-.macro hide_mugshot
-    call_hide_mugshot
-.endm
-
-.macro show_mugshot person alignment=MUGSHOT_LEFT message_type=MSG mask_name=0
-    load_mugshot \person \alignment \mask_name
-    show_mugshot_message \message_type
 .endm
