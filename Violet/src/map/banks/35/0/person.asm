3406e956e008230506b7f3f5d2fd794360e1de3b
Dominik Fuchsgruber <domi.erdnuss2@gmx.de>
2025-07-28 00:09:44 +0200

    trainersprites

--- Violet/src/map/banks/35/0/person.asm
+++ Violet/src/map/banks/35/0/person.asm
@@ -36,8 +36,19 @@ str_1:
 str_2:
     .autostring 34 2 "Hast du schonmal versucht, den Laz. Kessel zu benutzen?\pDamit kann man wirklich fabelhafte Items herstellen.\pNatürlich nur, wenn du auch die entsprechenden Rezepte kennstDOTS"
 str_3:
-    .autostring 34 2 "Theto ist so eine große Region.\pOhne eine Karte würde ich mich als Trainer gar nicht wirklich zurecht finden."
+    .autostring 34 2 "Wusstest du, dass man die Bücher in einem Pokéstop einfach mitnehmen darf?\pNatürlich wird von einem erwartet, dass man selbst ein anderes zurücklässt.\pAuf diese Weise bleibt die Sammlung immer aktuell!"
 str_4:
     .autostring 34 2 "Wiebitte?\nIch soll doch besser auf mein Pokémon Acht geben?\pIch lasse mir von so einem Dreikäsehoch doch nichts vorschreiben!"
 .elseif LANG_EN
+str_0:
+    .autostring 34 2 "Are you a Trainer?\pAre you going to challenge the Pokémon League?"
+str_1:
+    .autostring 34 2 "My friend here is a bit hot-headed.\pHe asks every Trainer if they want to challenge the Pokémon League.\pIt's very embarrassing for me."
+str_2:
+    .autostring 34 2 "Have you tried using the Laz. Kessel?\pIt can make really fabulous items.\pOf course, only if you know the right recipes."
+str_3:
+    .autostring 34 2 "Did you know that you can take the books from a Pokéstop?\pOf course, you're expected to leave another book in return.\pThis way, the collection stays up to date!"
+str_4:
+    .autostring 34 2 "What?\nI should take better care of my Pokémon?\pI won't take orders from some little brat!"
+
 .endif
